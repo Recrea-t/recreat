21b989bc8e110daa72edd507160a5cdc5430fa32
David Gay i Tello <david.gay.tello@protonmail.com>
2021-02-09 11:34:27 +0100

    disable identity widget on site

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,23 @@
-const path = require('path')
-const {createFilePath} = require('gatsby-source-filesystem')
+const path = require("path");
+const webpack = require(`webpack`);
+const { createFilePath } = require("gatsby-source-filesystem");
 
-exports.createPages = ({actions, graphql}) => {
-	const {createPage} = actions
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^netlify-identity-widget$/,
+      }),
+    ],
+  });
+};
 
-	return graphql(`
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  return graphql(`
     {
-      allMarkdownRemark(
-				limit: 1000
-			) {
+      allMarkdownRemark(limit: 1000) {
         edges {
           node {
             id
@@ -23,43 +32,43 @@ exports.createPages = ({actions, graphql}) => {
       }
     }
   `).then((result) => {
-		if (result.errors) {
-			result.errors.forEach((e) => console.error(e.toString()))
-			return Promise.reject(result.errors)
-		}
+    if (result.errors) {
+      result.errors.forEach((e) => console.error(e.toString()));
+      return Promise.reject(result.errors);
+    }
 
-		const posts = result.data.allMarkdownRemark.edges
+    const posts = result.data.allMarkdownRemark.edges;
 
-		posts.forEach((edge) => {
-			const id = edge.node.id
-			if (edge.node.frontmatter.templateKey === 'blog-page') {
-				return;
-			}
+    posts.forEach((edge) => {
+      const id = edge.node.id;
+      if (edge.node.frontmatter.templateKey === "blog-page") {
+        return;
+      }
 
-			createPage({
-				path: edge.node.fields.slug,
-				tags: edge.node.frontmatter.tags,
-				component: path.resolve(
-					`src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-				),
-				// additional data can be passed via context
-				context: {
-					id,
-				},
-			})
-		})
-	})
-}
+      createPage({
+        path: edge.node.fields.slug,
+        tags: edge.node.frontmatter.tags,
+        component: path.resolve(
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          id,
+        },
+      });
+    });
+  });
+};
 
-exports.onCreateNode = ({node, actions, getNode}) => {
-	const {createNodeField} = actions
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
 
-	if (node.internal.type === `MarkdownRemark`) {
-		const value = createFilePath({node, getNode})
-		createNodeField({
-			name: `slug`,
-			node,
-			value,
-		})
-	}
-}
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    });
+  }
+};
