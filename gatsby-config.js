04a32c952225c9cfc59fc4091b2c5f11751fbac9
David Gay i Tello <david.gay.tello@protonmail.com>
2021-03-19 10:32:41 +0100

    feat: adding gatsby-plugin-preload-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-robots-txt",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-preload-fonts",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
